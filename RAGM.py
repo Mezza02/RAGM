eb751ee28f51513b6328b96cecc243ee47fed928
Mezza02 <georgemezzone1@yahoo.co.uk>
2017-03-08 15:36:46 +0000

    Update RAGM.py

--- RAGM.py
+++ RAGM.py
@@ -16,6 +16,36 @@ Controls:
     return
 
 print("""Welcome to Ragm!""")
+print("=================")
+HELP = input("Press S to start or H for help")
+if HELP == "H":
+    print("=================")
+    print("Commands:")
+    print("      w = Forward")
+    print("      a = Left")
+    print("      s = Go back")
+    print("      d = Right")
+    print("      i = Inventory")
+    print("      y = Yes")
+    print("      n = No")
+    print("      r = Run")
+    print("      f = Fight")
+    print("=================")
+    HELP = input()
+
+else:
+    print("")
+
+print("=================")
+
+print("Let's Begin!!")
+print("=================")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("=================")
 print("""What is your name, young one?""")
 name = input("<~ ")
 while 1:
@@ -25,3 +55,7 @@ while 1:
     if (action == "h"):
 
         sendHelp()
+        
+        
+        
+    
